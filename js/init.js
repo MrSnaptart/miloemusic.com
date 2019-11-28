58414f68989d0b4886c9b077b1286e2f1ef1383f
Thomas Schroeder <mrsnaptart@gmail.com>
2019-11-28 10:04:12 -0600

    goood version

--- js/init.js
+++ js/init.js
@@ -1,5 +1,15 @@
 (function($){
 	$(function(){
+		
+	function validURL(str) {
+	  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+	    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+	    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+	    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+	    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+	    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+	  return !!pattern.test(str);
+	}
 
 	// $('.sidenav').sidenav();
 	// $('.parallax').parallax();
@@ -52,7 +62,7 @@
 			// Date
 			var date = data.items[i].start.dateTime.slice(5, 10).replace('-', '/');
 			if(date[3] == 0)
-				date = date[0] + date[1] + date[2] + date[4]
+				date = date[0] + date[1] + date[2] + date[4];
 
 			// Time
 			var time = data.items[i].start.dateTime.slice(11, 16);
@@ -69,20 +79,24 @@
 			}
 
 			// shows.push(date +  " " + lctn + " w/ " + acts + " " + time);
+			
+			
+				
+			var link = data.items[i].description;
 
-			var hidden = data.items[i].description
+			if(!validURL(link))
+				link = "https://www.instagram.com/miloemusic";
+		
 
-			if(data.items[i].description != "hidden") {
-				showStr = date +  " " + lctn + " w/ " + acts + " " + time;
-				$("#shows").append(
-					'<div class="row">' + 
-						'<div class="col s6 right-align mdate"><h6>' + mdate + '</h6></div>' + 
-						'<div class="col s6 left-align mdate"><h6>' + mtime + '</h6></div>' + 
-					
-						'<div class="col s6 right-align"><h6>' + lctn + '</h6></div>' +
-						'<div class="col s6 left-align"><h6>w/ ' + acts + '</h6></div>' +
-					'</div><br>');
-			}
+			$("#shows").append(
+				'<div class="row">' + 
+					'<div class="col s6 right-align mdate"><h6>' + mdate + '</h6></div>' + 
+					'<div class="col s6 left-align mdate"><h6>' + mtime + '</h6></div>' + 
+				
+					'<div class="col s6 right-align"><h6>' + '<a href="'+link+'" class="venue-link">'+lctn + '</a></h6></div>' +
+					'<div class="col s6 left-align"><h6>w/ ' + acts + '</h6></div>' +
+				'</div>');
+
 
 			/*<div class="row" >
 				<div class="col s1 offset-s1"><h6>2/14</h6></div>
